967714959c74154238c64fc2e8105745cbeaf2de
Christoph Champ <christoph.champ@gmail.com>
2019-05-01 15:08:37 -0700

    upaded helper script with more vars

--- push-image-to-ecr.sh
+++ push-image-to-ecr.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+AWS_ACCOUNT_ID=000000000000
 AWS_REGION=us-west-2
-CODECOMMIT_URL=https://git-codecommit.us-west-2.amazonaws.com/v1/repos/wotc-airflow-dags
-ECR_ENDPOINT=771502366784.dkr.ecr.us-west-2.amazonaws.com/wotc-airflow-dags
-IMAGE_NAME=wotc-airflow-dags
+IMAGE_NAME=my-image-name
 IMAGE_TAG=latest
+CODECOMMIT_URL=https://git-codecommit.${AWS_REGION}.amazonaws.com/v1/repos/${IMAGE_NAME}
+ECR_ENDPOINT=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}
 
 $(aws ecr get-login --no-include-email --region ${AWS_REGION})
 #docker build --no-cache --build-arg AWS_CODECOMMIT_URL=${CODECOMMIT_URL} -t ${IMAGE_NAME}:${IMAGE_TAG} .
